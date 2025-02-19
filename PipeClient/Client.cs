4ad65e3d6e97afd0f3b958a313eb388971da6a60
Bazhen Khudyakov <118445341+ThinGGsKoT@users.noreply.github.com>
2025-02-19 14:42:18 +0300

    Add files via upload

---
+++ PipeClient/Client.cs
@@ -0,0 +1,23 @@
+﻿using System.IO.Pipes;
+
+internal class Client
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("client");
+        NamedPipeClientStream namedPipeClientStream = new NamedPipeClientStream("chatPipe");
+        namedPipeClientStream.Connect();
+        StreamWriter streamWriter = new StreamWriter(namedPipeClientStream);
+        StreamReader streamReader = new StreamReader(namedPipeClientStream);
+        streamWriter.AutoFlush = true;
+
+        while (true)
+        {
+            string output = Console.ReadLine();
+            streamWriter.WriteLine(output);
+
+            string input = streamReader.ReadLine();
+            Console.WriteLine("Cервер ответил: " + input);
+        }
+    }
+}
---
+++ PipeServer/Server.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO.Pipes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("server");
+        Thread thread = new Thread(client);
+        thread.Start();
+    }
+    static List<string> msgs = new();
+    public static int count = -1;
+    public static void client()
+    {
+        count++;
+        Console.WriteLine("server current connestions: " + count);
+        NamedPipeServerStream namedPipeServerStream = new NamedPipeServerStream("chatPipe", PipeDirection.InOut, 10);
+        namedPipeServerStream.WaitForConnection();
+        Thread thread = new Thread(client);
+        thread.Start();
+        Console.WriteLine("Client is connected");
+
+        StreamReader streamReader = new StreamReader(namedPipeServerStream);
+        StreamWriter streamWriter = new StreamWriter(namedPipeServerStream);
+        int msgCount = 0;
+        while (true)
+        {
+            string msg = streamReader.ReadLine();
+            Console.WriteLine($"client {count} : {msg}");
+            msgs.Add(msg);
+            string send = "";
+            for (int i = msgCount; i < msgs.Count; i++)
+            {
+                send = send + msgs[i];
+            }
+            streamWriter.AutoFlush = true;
+            streamWriter.WriteLine($"сообщение от клиента {count} : {send}");
+            msgCount++;
+        }
+    }
+}
